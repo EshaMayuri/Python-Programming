e7d68f1c180065b6ed935c1b22f251fb2b484396
EshaMayuri <em7gh@mail.umkc.edu>
2017-06-15 17:55:24 -0500

    Lab 1

--- Lab1/Source/Random.py
+++ Lab1/Source/Random.py
@@ -1,6 +1,12 @@
 #importing library to use random number generator module
 import random
 
+print("Number guess game rules: ")
+print('''The system would pick a number by random and you will be given as many round as required until you guess the number correctly.
+If you guess the number in 1st round you would be awarded 10 points, which is the max number of points. 
+The number of turns you loose so many number of points would be deducted from you score of 10 for that round.''')
+
+score = 10
 #generating random number between 0 to 9
 rand = int(random.randint(0,9))
 #print(rand)
@@ -16,7 +22,6 @@ n = int(num)
 
 #displaying the result to user
 while(n!=rand):
-
     #checking if number guessed is less than the random number picked by the system
     if (n<rand):
         print("Sorry! number guessed by you is less than the number picked by the system.")
@@ -25,16 +30,18 @@ while(n!=rand):
     else:
         print("Sorry! number guessed is more than the number picked by the system.")
 
+    #incrementing the turn count
     count = count + 1
+
     # Asking the user to input the number that they have guessed
     print("Turn", count, ":")
     num = input("Guess the number picked by the system:")
 
     # Type casting the num to integer
     n = int(num)
-
 #checking if number guessed is exactly equal
 if (count == 1):
-    print("Congratulations! You have guessed the number correctly in 1 round")
+    print("Congratulations! You have guessed the number correctly in 1 round and your score is", score)
 else:
-    print("You have guessed the number correctly in", count, "rounds")
+    print("You have guessed the number correctly in", count, "rounds and your score is ",(score-count+1), ".")
+    print("So the number picked by the system is:", rand)
